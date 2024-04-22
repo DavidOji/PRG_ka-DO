414da68162bec63e62eae6ee895a38e7e56c517e
JuliBieber <115461993+JuliBieber@users.noreply.github.com>
2024-04-22 11:01:42 +0200

    Add files via upload
    iris_push

--- main.py
+++ main.py
@@ -1,39 +1,68 @@
-import tensorflow as tf
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 
-#Aufgabe 1
-path = "/Users/student/PycharmProjects/Clustering_SA/iris (1).csv"
-data = pd.read_csv(path, delimiter=',')
-print(data.head())
+# Datensatz laden
+df = pd.read_csv("iris.csv")
 
-#Aufgabe 3
-#x = [data['sepal.length']]
-#y = [data['sepal.width']]
+# Datensatz erkunden, ersten Zeilen anzeigen
+print(df.head())
+print(df.tail())
 
+# Zusammenfassung des DataFrames mit Informationen zu jeder Spalte
+print(df.info())
 
-#Aufgabe 4 (siehe auch OneNote'Clustering')
+# 20 Mal '#' ausgeben
+print("#" * 20)
 
-setosa = data[data['species'] == 'Setosa']
-versicolor = data[data['species'] == 'Versicolor']
-virginica = data[data['species'] == 'Virginica']
+# Spalten auswählen und die ersten 10 Zeilen anzeigen lassen
+selected_columns = df[['sepal.length', 'sepal.width']]
+print(selected_columns.head(10))
 
-#mögliche Lösung
-'''
-x = [setosa['sepal.length'], versicolor['sepal.length'], virginica['sepal.length']]
-y = [setosa['sepal.width'], versicolor['sepal.width'], virginica['sepal.width']]
+# Nur Zeilen mit 'setosa' anzeigen
+setosa = df[df['species'] =='Setosa']
+print(setosa)
 
-plt.scatter(x, y)
+# Alle Einträge 'petal.length'>1.5 und 'species' = 'Versicolor'
+versicolor = df[(df['petal.length']>1.5) & (df['species']=='Versicolor')]
+print(versicolor)
+
+# Fläche berechnen und als neue Spalte hinzufügen
+df['sepal.area'] = df['sepal.length'] * df['sepal.width']
+print(df)
+
+# Werte ändern, 'Setosa' zu 'S' usw...
+changed_species = df['species'].replace({'Setosa' : 'S', 'Versicolor' : 'Ve', 'Virginica': 'Vi'})
+print(changed_species)
+
+# Löschen der Zeilen bei denen 'sepal.length'<4.5
+deleted_lines = df[df['sepal.length']<4.5]
+print(deleted_lines)
+
+# Deskriptive Statistiken für numerische Spalten anzeigen (min, max)
+numeric_stats = df.describe().loc[['min', 'max']]
+print(numeric_stats)
+
+# Durchschnitt von 'sepal.length' gruppiert nach 'species'
+average_sepal_length = df.groupby('species')['sepal.length'].mean()
+print(average_sepal_length)
+
+# Einzigartige Werte der Spalte 'species' finden
+unique_species = df['species'].unique()
+print(unique_species)
+
+# Scatter plot erstellen farbkodiert nach 'species'
+plt.scatter(df['sepal.length'], df['sepal.width'])
+plt.xlabel('sepal.length')
+plt.ylabel('sepal.width')
 plt.show()
-'''
 
-#Lehrer-Lösung
-plt.scatter(setosa['sepal.length'], setosa['sepal.width'], color='red', marker='s')
-plt.scatter(versicolor['sepal.length'], versicolor['sepal.width'], color='blue', marker='o')
-plt.scatter(virginica['sepal.length'], virginica['sepal.width'], color='green', marker='^')
+setosa = df[df['species']=='Setosa']
+versicolor = df[df['species']=='Versicolor']
+virginica = df[df['species']=='Virginica']
 
+plt.scatter(setosa['sepal.length'], setosa['sepal.width'], color = 'red', marker='s')
+plt.scatter(versicolor['sepal.length'], versicolor['sepal.width'], color = 'blue', marker='o')
+plt.scatter(virginica['sepal.length'], virginica['sepal.width'], color = 'green',marker='^')
 plt.xlabel('sepal.length')
 plt.ylabel('sepal.width')
 plt.show()
